9d67ff418f2da1f97543879d156b594726b71a6d
lkb44 <99612103+lkb44@users.noreply.github.com>
2023-04-13 13:09:19 -0400

    Update CreateVolume.py

--- CreateVolume.py
+++ CreateVolume.py
@@ -1,49 +1,63 @@
-import os
 import numpy as np
+import os
 import pydicom
-import SimpleITK as sitk
+import cv2
 
-# define path to data folders
-data_path = "/Users/leobao/Library/CloudStorage/GoogleDrive-lkb44@case.edu/Shared drives/EBME 461 Project/Data"
+# Define HU window/level
+WINDOW_WIDTH = 350
+WINDOW_LEVEL = 40
 
-# loop through each patient folder
-for patient_folder in os.listdir(data_path):
-    if patient_folder.startswith("CA"):
-        # create path to image and label folders
-        image_folder = os.path.join(data_path, patient_folder, "Image")
-        label_folder = os.path.join(data_path, patient_folder, "Label")
-        
-        # get list of all image filenames in image folder
-        image_filenames = sorted([os.path.join(image_folder, filename) for filename in os.listdir(image_folder)])
-        
-        # get list of all label filenames in label folder
-        label_filenames = sorted([os.path.join(label_folder, filename) for filename in os.listdir(label_folder)])
-        
-        # loop through each slice and bisect
-        for i in range(len(image_filenames)):
-            # load the image and label slices
-            image_slice = pydicom.read_file(image_filenames[i]).pixel_array
-            label_slice = pydicom.read_file(label_filenames[i]).pixel_array
-            
-            # apply HU windowing
-            image_slice = np.clip(image_slice, -300, 600)
-            image_slice = (image_slice - (-300)) / (600 - (-300))
-            
-            # bisect the image and label slices
-            if i < len(image_filenames) // 2:
-                # bottom half of heart, sequence from bottom-to-middle
-                image_slice = image_slice[::-1]
-                label_slice = label_slice[::-1]
-                output_name = os.path.join(data_path, patient_folder + "_bottom", f"{patient_folder}_bottom_{i:03d}.mha")
-            else:
-                # top half of heart, sequence from top-to-middle
-                output_name = os.path.join(data_path, patient_folder + "_top", f"{patient_folder}_top_{i:03d}.mha")
-            
-            # save the image and label slices as a 3D volume
-            if not os.path.exists(os.path.dirname(output_name)):
-                os.makedirs(os.path.dirname(output_name))
-            sitk_image_slice = sitk.GetImageFromArray(image_slice)
-            sitk_label_slice = sitk.GetImageFromArray(label_slice)
-            sitk_image_slice.CopyInformation(sitk_label_slice)
-            sitk.WriteImage(sitk_image_slice, output_name)
-            print(f"Patient {patient_folder} bisected!")
+# Define the number of slices to bisect
+SLICES_TO_BISECT = 3
+
+# Define the output image size
+IMG_SIZE = 512
+
+input_path = "/Users/leobao/Library/CloudStorage/GoogleDrive-lkb44@case.edu/Shared drives/EBME 461 Project/Data"
+
+output_path = "/Users/leobao/Library/CloudStorage/GoogleDrive-lkb44@case.edu/Shared drives/EBME 461 Project/Output"
+
+def preprocess_images(input_path, output_path):
+    # Create output folder if it doesn't exist
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    # Loop through all patient folders
+    for patient_folder in os.listdir(input_path):
+        patient_path = os.path.join(input_path, patient_folder)
+
+        # Loop through all image slices for the current patient
+        for i in range(len(os.listdir(os.path.join(patient_path, "Image")))):
+            # Read image slice and apply HU window/level
+            img_path = os.path.join(patient_path, "Image", "IMG{:03d}.dcm".format(i+1))
+            ds = pydicom.dcmread(img_path)
+            img = ds.pixel_array.astype(np.float32)
+            img = np.clip(img, WINDOW_LEVEL-WINDOW_WIDTH/2, WINDOW_LEVEL+WINDOW_WIDTH/2)
+            img = (img - (WINDOW_LEVEL-WINDOW_WIDTH/2)) / (WINDOW_WIDTH)
+            img = np.uint8(img * 255)
+
+            # Bisection
+            if i < SLICES_TO_BISECT:
+                continue
+            img_slices = []
+            for j in range(i-SLICES_TO_BISECT, i+1):
+                img_path = os.path.join(patient_path, "Image", "IMG{:03d}.dcm".format(j+1))
+                ds = pydicom.dcmread(img_path)
+                img_slice = ds.pixel_array.astype(np.float32)
+                img_slice = np.clip(img_slice, WINDOW_LEVEL-WINDOW_WIDTH/2, WINDOW_LEVEL+WINDOW_WIDTH/2)
+                img_slice = (img_slice - (WINDOW_LEVEL-WINDOW_WIDTH/2)) / (WINDOW_WIDTH)
+                img_slice = np.uint8(img_slice * 255)
+                img_slices.append(img_slice)
+
+            # Concatenate images
+            img_concat = cv2.hconcat(img_slices)
+
+            # Resize image to IMG_SIZE x IMG_SIZE
+            img_resized = cv2.resize(img_concat, (IMG_SIZE, IMG_SIZE), interpolation = cv2.INTER_AREA)
+
+            # Save image to output folder
+            output_filename = "IMG{:03d}.png".format(i+1)
+            output_filepath = os.path.join(output_path, patient_folder, output_filename)
+            if not os.path.exists(os.path.dirname(output_filepath)):
+                os.makedirs(os.path.dirname(output_filepath))
+            cv2.imwrite(output_filepath, img_resized)
