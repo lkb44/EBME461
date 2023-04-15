597c1be81377a027c2d511d7bbca1066d9b6ed5d
lkb44 <99612103+lkb44@users.noreply.github.com>
2023-04-15 11:48:55 -0400

    Update CreateVolume.py

--- CreateVolume.py
+++ CreateVolume.py
@@ -1,49 +1,66 @@
 import os
 import numpy as np
+import nibabel as nib
 import pydicom
-import SimpleITK as sitk
+from skimage import exposure
 
-# define path to data folders
-data_path = "/Users/leobao/Library/CloudStorage/GoogleDrive-lkb44@case.edu/Shared drives/EBME 461 Project/Data"
+def apply_preprocessing(img):
+    # Apply 350 HU/40 HU window/level operation
+    img = exposure.rescale_intensity(img, in_range=(40, 390), out_range=(0, 1))
+    img = exposure.adjust_gamma(img, gamma=0.8)
+    img = np.uint8(img * 255)
+    return img
 
-# loop through each patient folder
-for patient_folder in os.listdir(data_path):
-    if patient_folder.startswith("CA"):
-        # create path to image and label folders
-        image_folder = os.path.join(data_path, patient_folder, "Image")
-        label_folder = os.path.join(data_path, patient_folder, "Label")
-        
-        # get list of all image filenames in image folder
-        image_filenames = sorted([os.path.join(image_folder, filename) for filename in os.listdir(image_folder)])
-        
-        # get list of all label filenames in label folder
-        label_filenames = sorted([os.path.join(label_folder, filename) for filename in os.listdir(label_folder)])
-        
-        # loop through each slice and bisect
-        for i in range(len(image_filenames)):
-            # load the image and label slices
-            image_slice = pydicom.read_file(image_filenames[i]).pixel_array
-            label_slice = pydicom.read_file(label_filenames[i]).pixel_array
-            
-            # apply HU windowing
-            image_slice = np.clip(image_slice, -300, 600)
-            image_slice = (image_slice - (-300)) / (600 - (-300))
-            
-            # bisect the image and label slices
-            if i < len(image_filenames) // 2:
-                # bottom half of heart, sequence from bottom-to-middle
-                image_slice = image_slice[::-1]
-                label_slice = label_slice[::-1]
-                output_name = os.path.join(data_path, patient_folder + "_bottom", f"{patient_folder}_bottom_{i:03d}.mha")
-            else:
-                # top half of heart, sequence from top-to-middle
-                output_name = os.path.join(data_path, patient_folder + "_top", f"{patient_folder}_top_{i:03d}.mha")
-            
-            # save the image and label slices as a 3D volume
-            if not os.path.exists(os.path.dirname(output_name)):
-                os.makedirs(os.path.dirname(output_name))
-            sitk_image_slice = sitk.GetImageFromArray(image_slice)
-            sitk_label_slice = sitk.GetImageFromArray(label_slice)
-            sitk_image_slice.CopyInformation(sitk_label_slice)
-            sitk.WriteImage(sitk_image_slice, output_name)
-            print(f"Patient {patient_folder} bisected!")
+def process_patient(patient_dir):
+    # Get list of DICOM files for Image folder
+    img_folder = os.path.join(patient_dir, "Image")
+    img_files = sorted([f for f in os.listdir(img_folder) if f.endswith(".dcm")])
+
+    # Divide heart slices into two equal halves
+    img_files_lower = img_files[:len(img_files)//2]
+    img_files_upper = img_files[len(img_files)//2:]
+
+    # Process slices for lower half of heart
+    img_slices_lower = []
+    for f in img_files_lower:
+        dicom_file = pydicom.dcmread(os.path.join(img_folder, f))
+        img_slice = apply_preprocessing(dicom_file.pixel_array)
+        img_slices_lower.append(img_slice)
+
+    # Process slices for upper half of heart
+    img_slices_upper = []
+    for f in img_files_upper:
+        dicom_file = pydicom.dcmread(os.path.join(img_folder, f))
+        img_slice = apply_preprocessing(dicom_file.pixel_array)
+        img_slices_upper.append(img_slice)
+
+    # Generate input voxel slabs by concatenating 3 consecutive slices
+    slabs = []
+    for i in range(len(img_slices_lower)-2):
+        slab = np.zeros((512, 512, 3), dtype=np.uint8)
+        slab[..., 0] = img_slices_lower[i]
+        slab[..., 1] = img_slices_lower[i+1]
+        slab[..., 2] = img_slices_lower[i+2]
+        slabs.append(slab)
+    for i in range(len(img_slices_upper)-2):
+        slab = np.zeros((512, 512, 3), dtype=np.uint8)
+        slab[..., 0] = img_slices_upper[i]
+        slab[..., 1] = img_slices_upper[i+1]
+        slab[..., 2] = img_slices_upper[i+2]
+        slabs.append(slab)
+
+    # Concatenate slabs to form 3D volume
+    volume = np.concatenate(slabs, axis=2)
+
+    # Save volume in NifTI format
+    output_dir = os.path.join(patient_dir, "Output")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_file = os.path.join(output_dir, "volume.nii.gz")
+    nib.save(nib.Nifti1Image(volume, np.eye(4)), output_file)
+
+# Process all patients
+parent_dir = "/Users/leobao/Library/CloudStorage/GoogleDrive-lkb44@case.edu/Shared drives/EBME 461 Project/Data/"
+patient_dirs = sorted([os.path.join(parent_dir, f"CA{i:03}") for i in range(1, 47)])
+for patient_dir in patient_dirs:
+    process_patient(patient_dir)
