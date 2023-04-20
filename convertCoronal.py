287bb4ed9bac0674bee517c4ffff61ca8428d7a3
lkb44 <99612103+lkb44@users.noreply.github.com>
2023-04-20 01:28:48 -0400

    redo output

--- convertCoronal.py
+++ convertCoronal.py
@@ -9,8 +9,8 @@ for patient_folder in patient_folders:
     print(f"Converting {patient_folder}...")
 
     # Load the axial image and label volumes
-    image_path = os.path.join(parent_path, patient_folder, "Output", f"{patient_folder}_image_volume.nii.gz")
-    label_path = os.path.join(parent_path, patient_folder, "Output", f"{patient_folder}_label_volume.nii.gz")
+    image_path = os.path.join(parent_path, patient_folder, "Output_redo", f"{patient_folder}_image_volume.nii.gz")
+    label_path = os.path.join(parent_path, patient_folder, "Output_redo", f"{patient_folder}_label_volume.nii.gz")
     axial_image = nib.load(image_path)
     axial_label = nib.load(label_path)
 
@@ -27,7 +27,7 @@ for patient_folder in patient_folders:
     coronal_label = nib.Nifti1Image(coronal_label_data, affine=axial_label.affine)
 
     # Save the new NIfTI image and label objects as .nii.gz files
-    output_folder = os.path.join(parent_path, patient_folder, "Output")
+    output_folder = os.path.join(parent_path, patient_folder, "Output_redo")
     nib.save(coronal_image, os.path.join(output_folder, f"{patient_folder}_image_volume_coronal.nii.gz"))
     nib.save(coronal_label, os.path.join(output_folder, f"{patient_folder}_label_volume_coronal.nii.gz"))
 
