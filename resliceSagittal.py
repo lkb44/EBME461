3058c976d7f304078b26eac8f9fd41d10a7cec0a
lkb44 <99612103+lkb44@users.noreply.github.com>
2023-04-21 01:04:34 -0400

    edits

--- resliceSagittal.py
+++ resliceSagittal.py
@@ -19,24 +19,24 @@ for patient_folder in patient_order:
     label_volume = sitk.ReadImage(label_volume_path)
     image_volume = sitk.ReadImage(image_volume_path)
         
-    # reslice the volumes to the coronal plane
+    # reslice the volumes to the sagittal plane
     reslice_filter = sitk.ResampleImageFilter()
-    reslice_filter.SetOutputDirection((-1,0,0,0,0,1,0,1,0))
+    reslice_filter.SetOutputDirection((0,0,-1,-1,0,0,0,-1,0))
     reslice_filter.SetOutputSpacing((1,1,1))
     reslice_filter.SetOutputOrigin(image_volume.GetOrigin())
-    reslice_filter.SetSize((512, 512, image_volume.GetDepth()))
-    coronal_label_volume = reslice_filter.Execute(label_volume)
-    coronal_image_volume = reslice_filter.Execute(image_volume)
+    reslice_filter.SetSize((512, image_volume.GetHeight(), 512))
+    sagittal_label_volume = reslice_filter.Execute(label_volume)
+    sagittal_image_volume = reslice_filter.Execute(image_volume)
         
-    # save the coronal slices as DICOM files
-    coronal_images_path = os.path.join(parent_path, patient_folder, 'Output_2', 'Coronal_Images')
-    coronal_labels_path = os.path.join(parent_path, patient_folder, 'Output_2', 'Coronal_Labels')
-    os.makedirs(coronal_images_path, exist_ok=True)
-    os.makedirs(coronal_labels_path, exist_ok=True)
-    for i in range(coronal_image_volume.GetSize()[2]):
-        image_slice = sitk.GetArrayViewFromImage(coronal_image_volume)[..., i]
-        label_slice = sitk.GetArrayViewFromImage(coronal_label_volume)[..., i]
-        image_path = os.path.join(coronal_images_path, f"IMG{i+1:04}.dcm")
-        label_path = os.path.join(coronal_labels_path, f"LBL{i+1:04}.dcm")
+    # save the sagittal slices as DICOM files
+    sagittal_images_path = os.path.join(parent_path, patient_folder, 'Output_2', 'Sagittal_Images')
+    sagittal_labels_path = os.path.join(parent_path, patient_folder, 'Output_2', 'Sagittal_Labels')
+    os.makedirs(sagittal_images_path, exist_ok=True)
+    os.makedirs(sagittal_labels_path, exist_ok=True)
+    for i in range(sagittal_image_volume.GetSize()[0]):
+        image_slice = sitk.GetArrayViewFromImage(sagittal_image_volume)[i, ...]
+        label_slice = sitk.GetArrayViewFromImage(sagittal_label_volume)[i, ...]
+        image_path = os.path.join(sagittal_images_path, f"IMG{i+1:04}.dcm")
+        label_path = os.path.join(sagittal_labels_path, f"LBL{i+1:04}.dcm")
         sitk.WriteImage(sitk.GetImageFromArray(image_slice), image_path, True)
         sitk.WriteImage(sitk.GetImageFromArray(label_slice), label_path, True)
